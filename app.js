3277bf09679a6b2258606fdb4c6f1c4ff9ee2415
José Miguel <Tapuy@hotmail.es>
2024-08-13 17:43:48 +0200

    first connections

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PORT = 9000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Inicio de la conexión a Mongo DB
+require("dotenv").config();
+const url_mongo = process.env.DATABASE_URL_DEV;
+mongoose.connect(url_mongo);
+
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.log(`Error en la conexión a la Base de datos: ${error}`);
+});
+
+db.on("connected", () => {
+  console.log(`La conexión a mongo se ha realizado con éxito`);
+});
+
+db.on("disconnected", () => {
+  console.log(`Se perdió la conexión a la Base de datos`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ mock/charactersDb.js
@@ -0,0 +1,68 @@
+let characters = [
+  {
+    nombre: "Arthur Morgan",
+    fechaNacimiento: "1863",
+    fechaMuerte: "1899",
+    faccion: "Van der Linde",
+    descripcion:
+      "Arthur Morgan es el protagonista principal de Red Dead Redemption 2. Es el lugarteniente de la banda de Dutch Van der Linde y uno de los miembros más antiguos y leales.",
+  },
+  {
+    nombre: "Dutch van der Linde",
+    fechaNacimiento: "1855",
+    fechaMuerte: "1911",
+    faccion: "Van der Linde",
+    descripcion:
+      "Dutch van der Linde es el líder carismático y visionario de la banda Van der Linde. Es un hombre idealista pero peligroso que cree en la libertad individual por encima de todo.",
+  },
+  {
+    nombre: "John Marston",
+    fechaNacimiento: "1873",
+    fechaMuerte: "1911",
+    faccion: "Van der Linde",
+    descripcion:
+      "John Marston es un ex-miembro de la banda Van der Linde y el protagonista de Red Dead Redemption. Después de los eventos de Red Dead Redemption 2, intenta llevar una vida pacífica con su familia.",
+  },
+  {
+    nombre: "Sadie Adler",
+    fechaNacimiento: "1850",
+    fechaMuerte: "Desconocido",
+    faccion: "Van der Linde",
+    descripcion:
+      "Sadie Adler es una viuda que se une a la banda Van der Linde después de que su esposo es asesinado por una banda rival. Se convierte en una cazarrecompensas feroz y leal a Arthur Morgan.",
+  },
+  {
+    nombre: "Micah Bell",
+    fechaNacimiento: "1860",
+    fechaMuerte: "1899",
+    faccion: "Van der Linde",
+    descripcion:
+      "Micah Bell es un miembro de la banda Van der Linde y uno de los principales antagonistas de Red Dead Redemption 2. Es astuto, despiadado y traicionero.",
+  },
+  {
+    nombre: "Charles Smith",
+    fechaNacimiento: "1843",
+    fechaMuerte: "Desconocido",
+    faccion: "Van der Linde",
+    descripcion:
+      "Charles Smith es un miembro medio afroamericano y medio nativo americano de la banda Van der Linde. Es conocido por su fuerza y habilidades de combate, así como por su sentido de la justicia.",
+  },
+  {
+    nombre: "Hosea Matthews",
+    fechaNacimiento: "1844",
+    fechaMuerte: "1899",
+    faccion: "Van der Linde",
+    descripcion:
+      "Hosea Matthews es el cofundador y uno de los miembros más antiguos de la banda Van der Linde. Es un estafador astuto y actúa como la voz de la razón para Dutch.",
+  },
+  {
+    nombre: "Bill Williamson",
+    fechaNacimiento: "1866",
+    fechaMuerte: "1911",
+    faccion: "Van der Linde",
+    descripcion:
+      "Bill Williamson es un ex-soldado y miembro leal de la banda Van der Linde. Es conocido por su temperamento explosivo y su lealtad hacia Dutch.",
+  },
+];
+
+module.exports = characters;
